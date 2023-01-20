c18d0fd6b684c31eeb213fdaaaf8388d790e8101
ugoflorent <ugo.florent15@gmail.com>
2023-01-20 16:43:50 +0100

    first commit

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ mongoose.connect(process.env.MONGODB_URI);
 
 cloudinary.config({
   cloud_name: process.env.CLOULDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
 
